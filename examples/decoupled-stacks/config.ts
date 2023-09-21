9c40d3d322dbcd3af60a2fa585cb2b7e388ebbda
Sanjay Kumar <sanjayshekhar@live.com>
2023-09-21 23:45:35 +0530

    Added separated stack to example for RS env

---
+++ examples/decoupled-stacks/config.ts
@@ -0,0 +1,3 @@
+export const reconcilerMergedBucketArn="arn:aws:s3:::xxxxxxxxxxxxxxxxx";
+export const reconcilerAPISchemaBucketArn= "arn:aws:s3:::xxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxx";
+export const centralAPIGW="arn:aws:execute-api:ap-south-1:xxxxxxxxxxxxxxxxx:xxxxxxxxxxxxxxxxx/*/*/"
---
+++ examples/new-rs-stack/config.ts
@@ -0,0 +1,8 @@
+export const reconcilerMergedBucketArn="arn:aws:s3:::xxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxx";
+export const reconcilerAPISchemaBucketArn= "arn:aws:s3:::xxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxx";
+export const centralAPIGW="arn:aws:execute-api:ap-southeast-1:xxxxxxxxxxxxxxxxx:xxxxxxxxxxxxxxxxx/*/*/"
+
+export const centralAPIGWId ="xxxxxxxxxxxxxxxxx";
+export const centralAPIGWrootResourceId= "xxxxxxxxxxxxxxxxx";
+export const authServiceAccountId="xxxxxxxxxxxxxxxxx";
+export const demoServiceAccountId="xxxxxxxxxxxxxxxxx";
